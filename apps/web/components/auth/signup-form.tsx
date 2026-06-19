ab8f94ac6a261c29cda847d008ab75c6555df862
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2026-06-19 15:30:32 -0600

    Added form component from shadcn and added props to signup-form.ts with zod from schema.ts

--- apps/web/components/auth/signup-form.tsx
+++ apps/web/components/auth/signup-form.tsx
@@ -1,6 +1,36 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+'use client';
 
-export default function SignupForm() {
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { SignupFormData, signupSchema } from "@/lib/auth/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+interface SignupFormProps {
+  OnSubmit: (data: SignupFormData) => Promise<void>;
+}
+
+export default function SignupForm({ OnSubmit }: SignupFormProps) {
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<SignupFormData>({
+    resolver: zodResolver(signupSchema),
+  });
+
+  const handleSubmit = async (data: SignupFormData) => {
+    setIsSubmitting(true);
+    try {
+      await OnSubmit(data);
+    } catch (error) {
+      console.error("Signup Error:", error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+
   return (
     <Card className="w-full max-w-md mx-auto">
       <CardHeader>
@@ -9,7 +39,24 @@ export default function SignupForm() {
           Enter your information to create a new account
         </CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl></FormControl>                
+                </FormItem>
+              )}
+            >
+
+            </FormField>
+          </form>
+        </Form>
+      </CardContent>
     </Card>
   )
 }
