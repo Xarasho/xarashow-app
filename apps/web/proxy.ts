59fad2e30eadc2e09bcf4a652b93ec64c41512e8
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2026-06-20 14:01:32 -0600

    Added the Proxy (middleware) to the web app

---
+++ apps/web/proxy.ts
@@ -0,0 +1,28 @@
+// Middleware -> Proxy
+
+import { getSessionCookie } from "better-auth/cookies";
+import { NextRequest, NextResponse } from "next/server";
+
+const publicRoutes = ["/signup", "/login"];
+
+export default function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|api).*)",
+  ]
+};
